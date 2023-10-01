76107339321d8d3df68bfd90799b6b9449a74e4e
sayam9901 <sayamkhan9560621912@gmail.com>
2023-10-01 20:38:07 +0530

    adding about page with some styling

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import "./contact.css";
 
 export const Contact = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-page">
+    <div className="background-image"></div>
+    <div className="content">
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula urna nec massa bibendum, nec dapibus
+        arcu tincidunt. Nam et fringilla justo. Vivamus congue elit id purus hendrerit, in dignissim purus consequat.
+        Sed vel massa vitae eros feugiat feugiat. Cras viverra hendrerit lectus, nec commodo justo. Suspendisse in
+        libero nec diam iaculis hendrerit non vel nulla. Donec tincidunt, justo ac feugiat auctor, urna lectus
+        iaculis lectus, bibendum rhoncus ipsum nulla vitae eros.
+      </p>
+      <p>
+        Integer euismod odio in eros eleifend tincidunt. Vivamus at lectus tincidunt, volutpat tellus ut, fermentum
+        libero. Fusce hendrerit varius venenatis. Nullam scelerisque sapien vel odio ultrices, in lacinia ipsum
+        feugiat. Nullam condimentum efficitur quam, non tempus mi. Quisque consectetur euismod odio non bibendum. Etiam
+        malesuada sapien in neque iaculis, nec fringilla justo fermentum. Suspendisse potenti. Proin placerat purus in
+        posuere gravida. Nullam at auctor quam, et tincidunt metus. Vivamus suscipit malesuada ex, vel varius augue.
+        Phasellus nec urna vel odio iaculis sollicitudin. Vestibulum lacinia nunc vitae nunc auctor, at ultricies mi
+        facilisis.
+      </p>
+    </div>
+  </div>
+  );
 };
