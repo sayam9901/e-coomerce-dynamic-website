dabd28f0c67f442e04f725786df1e9d8ad3411b4
sayam9901 <sayamkhan9560621912@gmail.com>
2023-10-02 20:08:48 +0530

    home page implemetation

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import "./hero.css"
 
-export const Hero =()=>{
-    return <div>home is here baby</div>;
+const Data = [
+  {
+    date: "AUG 3",
+    city: "mumbai",
+    place: "garden",
+  },
+  { date: "AUG 3", city: "mumbai", place: "garden" },
+  { date: "AUG 3", city: "mumbai", place: "garden" },
+  { date: "AUG 3", city: "mumbai", place: "garden" },
+  { date: "AUG 3", city: "mumbai", place: "garden" },
+];
+
+export const Hero = () => {
+  return (
+    <div className="container">
+      <div>
+        <h1>GET OUR LATEST ALBUM</h1>
+        <button>PLAY</button>
+      </div>
+      <div>TOURS</div>
+      <ul>
+        {Data.map((item, index) => (
+          <li key={index}>
+            <strong>Date:</strong> {item.date}, <strong>City:</strong> {item.city}, <strong>Place:</strong> {item.place}
+            <button>BUY TICKETS</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
