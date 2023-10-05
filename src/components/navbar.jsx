073615f8bb494eadbbbf97f55055b861980e96c0
sayam9901 <sayamkhan9560621912@gmail.com>
2023-10-05 23:34:59 +0530

    implemeting the setimout for logout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState , useEffect} from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import { signOut } from "firebase/auth";
@@ -14,6 +14,41 @@ export const Navbar = () => {
         history('/login')
     })
 }
+  // // Function to reset the logout timer
+  // const resetLogoutTimer = () => {
+  //   // Clear the previous timer if it exists
+  //   if (logoutTimer) {
+  //     clearTimeout(logoutTimer);
+  //   }
+
+  //   // Set a new timer to log the user out after 5 minutes (300,000 milliseconds)
+  //   const newTimer = setTimeout(() => {
+  //     handleLogout();
+  //   }, 300000); // 5 minutes in milliseconds
+
+  //   // Update the timer state
+  //   setLogoutTimer(newTimer);
+  // };
+
+  // // Add an event listener to reset the timer on user activity
+  // useEffect(() => {
+  //   const resetTimerOnUserActivity = () => {
+  //     resetLogoutTimer();
+  //   };
+
+  //   // Listen for user activity events such as clicks, keypresses, etc.
+  //   window.addEventListener("click", resetTimerOnUserActivity);
+  //   window.addEventListener("keypress", resetTimerOnUserActivity);
+
+  //   // Initialize the timer when the component mounts
+  //   resetLogoutTimer();
+
+  //   // Cleanup the event listeners when the component unmounts
+  //   return () => {
+  //     window.removeEventListener("click", resetTimerOnUserActivity);
+  //     window.removeEventListener("keypress", resetTimerOnUserActivity);
+  //   };
+  // }, []);
   return (
     <div className="navbar">
       <div className="links">
